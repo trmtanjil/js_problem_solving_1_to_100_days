a2773f0ace30e80eb728fb2c088543322cf338e2
trmtanjil <trmtanjil02@gmail.com>
2025-12-14 21:14:22 +0600

    first commit

---
+++ ProblemsolvDay1/RemoveFalsyvaluefromeArry.js
@@ -0,0 +1,5 @@
+
+const boleen= ['tanjil',false, undefined, NaN, '100', ]
+
+const truee = boleen.filter(Boolean)
+console.log(truee)
---
+++ ProblemsolvDay1/convertvalueBolean.js
@@ -0,0 +1,3 @@
+console.log(!!'tanjil')
+console.log(!!0)
+console.log(!!11)
