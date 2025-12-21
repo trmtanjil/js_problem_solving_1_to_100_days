cd7fd3baa7949e099f175566140f55eeb3f359cc
trmtanjil <trmtanjil02@gmail.com>
2025-12-21 21:18:35 +0600

    commit

--- problemSolvDay6/minimumSum4digit.js
+++ problemSolvDay6/minimumSum4digit.js
@@ -1,5 +1,6 @@
 const minimumSum= function(num){
     const arr = num.toString().split('').sort();
+    console.log(arr)
     const sum = Number(arr[0]+arr[2])+ Number(arr[1]+arr[3])
     return sum;
 }
